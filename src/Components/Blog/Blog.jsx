d50d15a9b63d4455b3abde2d022743c2d660135f
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 18:36:29 +0600

    Added bookmarks.jsx

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types';
 
-const Blog = () => {
+
+const Blog = ({blog}) => {
+    console.log(blog);
     return (
         <div>
         </div>
     );
 };
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
