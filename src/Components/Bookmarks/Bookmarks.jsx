43cd705dc84b346d31e0dce2a043cd70328026ff
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 20:11:13 +0600

    Added Bookmark.jsx

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,14 +1,18 @@
-
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 const Bookmarks = ({bookmarks}) => {
     return (
         <div className="md:1/3">
             <h2>Bookmarked Blogs: {bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
 
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array.isRequired
+    bookmarks: PropTypes.array
 }
 
 export default Bookmarks;
