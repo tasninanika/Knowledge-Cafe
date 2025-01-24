4440a7907c564d49503588f933f9f18c3105ec05
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 18:17:32 +0600

    Updated Blogs.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
         <div>
             <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
