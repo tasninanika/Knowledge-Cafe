fe55b86175680c6c03c3ac10c63bb298e081948d
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 19:05:52 +0600

    Updated Blog.jsx

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
     const {title,cover,author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img src={cover} alt={`Cover picture of the title ${title}`} className='w-full mb-8'/>
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img src={author_img} alt="" className='w-14'/>
                     <div className='ml-6'>
