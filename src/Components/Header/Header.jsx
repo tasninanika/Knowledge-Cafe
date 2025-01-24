430f3948e46b0e65a629c0c374e99efbf5022b6e
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 21:48:24 +0600

    updated app.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <header className='flex justify-between items-center p-4 border-b border-gray-300 max-w-7xl mx-auto'>
+        <header className='flex justify-between items-center p-4 border-b border-gray-300 max-w-7xl mx-auto mt-4'>
             <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
             <img src={profile} alt="" />
         </header>
