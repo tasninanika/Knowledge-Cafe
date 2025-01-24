8b0df1e61b52e18fd967079c0d6966e3d09b15cf
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 19:56:55 +0600

    Added Bookmark.jsx

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -1,10 +1,18 @@
+import PropTypes from "prop-types";
 
-const Bookmark = () => {
+const Bookmark = ({bookmark}) => {
+
+    const {title} = bookmark;
+
     return (
         <div>
-            
+            <h3 className="text-3xl">{title}</h3>
         </div>
     );
 };
+
+Bookmark.propTypes ={
+    bookmark: PropTypes.object
+}
 
 export default Bookmark;
