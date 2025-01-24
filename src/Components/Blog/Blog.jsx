a3043e3cf1bf69ca407eb01dc4e8445bb29a92f7
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 18:59:18 +0600

    Updated Blogs.jsx

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types';
 
 
 const Blog = ({blog}) => {
-    const {title,cover,author, author_img, posted_date, reading_time} = blog;
+    const {title,cover,author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            <img src={cover} alt={`Cover picture of the title${title}`} />
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between'>
-                <div>
+                <div className='flex'>
                     <img src={author_img} alt="" className='w-14'/>
-                    <div>
+                    <div className='ml-6'>
                         <h3 className='text-2xl'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
@@ -19,6 +19,8 @@ const Blog = ({blog}) => {
                 </div>
             </div>
             <h1 className='text-4xl'>Blogs: {title}</h1>
+            <p>{
+                hashtags.map((hash, idx)=> <span key={idx} > {hash}</span>)}</p>
         </div>
     );
 };
